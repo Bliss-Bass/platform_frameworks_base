fd2185ac539a2af3f7598156574f7504646e1a01
Jon West <electrikjesus@gmail.com>
2024-07-03 17:47:38 -0400

    Revert "fwb: Limit app installation on lockdown mode"
    This reverts commit 3086fe0148f270f32b3ffe4035e27483974a78ea.

--- packages/PackageInstaller/src/com/android/packageinstaller/PackageInstallerActivity.java
+++ packages/PackageInstaller/src/com/android/packageinstaller/PackageInstallerActivity.java
@@ -44,7 +44,6 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Process;
-import android.os.SystemProperties;
 import android.os.UserManager;
 import android.provider.Settings;
 import android.util.Log;
@@ -447,10 +446,6 @@ public class PackageInstallerActivity extends AlertActivity {
      * show the appropriate dialog.
      */
     private void checkIfAllowedAndInitiateInstall() {
-        if (SystemProperties.get("ro.boot.bliss.bootmode").equals("lockdown")) {
-            showDialogInner(DLG_UNKNOWN_SOURCES_RESTRICTED_FOR_USER);
-            return;
-        }
         // Check for install apps user restriction first.
         final int installAppsRestrictionSource = mUserManager.getUserRestrictionSource(
                 UserManager.DISALLOW_INSTALL_APPS, Process.myUserHandle());
